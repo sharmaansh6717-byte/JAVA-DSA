4e61d1edb82002f23e65a6b7643921bb1659aa22
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-19 17:06:50 +0000

    array even digit counting

---
+++ Array_que/Array_01.java
@@ -0,0 +1,40 @@
+class Array_01 {
+     public int findNumbers(int[] nums) {
+         int evencounter = 0;
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            int num = nums[i];
+            int digit = countDigits(num);
+            if(digit % 2 == 0)
+            {
+                evencounter++;
+            }
+            else
+            {
+                continue;
+            }
+           
+        }
+         return evencounter;
+     }
+        
+   
+
+   
+    int countDigits(int num)
+    {
+       int counter = 0 ;
+       int digit = num ;
+        while(digit!=0)
+        {
+            digit /= 10;
+            counter++;
+            
+        }
+        return counter;
+     
+        
+        
+    }
+   
+}

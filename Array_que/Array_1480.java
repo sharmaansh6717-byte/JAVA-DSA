ba9cfa86fcceddf5af6251641062c6dcfdcbb131
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-20 19:08:10 +0000

    Running Sum of 1d Array

---
+++ Array_que/Array_1480.java
@@ -0,0 +1,29 @@
+//Brutte 
+// // public class Array_1480 {
+//     public int[] runningSum(int[] nums) {
+//         int[] runningSum = new int[nums.length];
+//         for(int i = 0; i < nums.length ; i++)
+//         {
+//            for(int j = 0 ; j <= i ; j++)
+//            {
+//             runningSum[i] += nums[j];
+//            }
+//         }
+//         return runningSum;
+//     }
+    
+// }
+
+
+//Optimized
+public class Array_1480 {
+    public int[] runningSum(int[] nums) {   
+        int[] runningSum = new int[nums.length];
+        runningSum[0] = nums[0];
+        for(int i = 1; i < nums.length ; i++)
+        {
+            runningSum[i] = runningSum[i-1] + nums[i];
+        }
+        return runningSum;
+    }
+}

93c64b0a285edf38c3c086f6f4a6e83a37c6420f
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-24 16:19:21 +0000

    space change

--- Array_que/Array_1480.java
+++ Array_que/Array_1480.java
@@ -22,7 +22,7 @@ public class Array_1480 {
         runningSum[0] = nums[0];
         for(int i = 1; i < nums.length ; i++)
         {
-            runningSum[i] = runningSum[i-1] + nums[i];
+            runningSum[i] = runningSum[ i-1 ] + nums[ i ];
         }
         return runningSum;
     }

cec6d7bc8fc3e20b8a6df3bff2fad1822bf2f491
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-25 17:53:18 +0000

    richest customer array optimized and brutte approach

--- Array_que/Array_1672.java
+++ Array_que/Array_1672.java
@@ -1,3 +1,55 @@
 public class Array_1672 {
-    /// 
+    //BRUTTE APPROACH;
+    public int maximumWealth(int[][] accounts){
+    
+//     int[] sum = new int[accounts.length];
+//     int summ = 0;
+//      {
+//         for(int i = 0 ; i < accounts.length; i++)
+//         {
+//             for(int j = 0; j < accounts[i].length; j++)
+//             {
+//               summ += accounts[i][j];
+//               sum[i] = summ;
+              
+//             }
+//             summ = 0;
+           
+//         }
+//         int temp = sum[0];
+//         for(int i = 1 ; i < accounts.length ; i++)
+//         {
+            
+//             if(sum[i] > temp)
+//             {
+//                 temp = sum[i];
+//             }
+//         }
+//         return temp;
+        
+//     }
+// }
+//OPTIMMIZED
+ int max = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+
+            int sum = 0;
+
+            for (int j = 0; j < accounts[i].length; j++) {
+
+                sum += accounts[i][j];
+
+            }
+
+            if (sum > max) {
+
+                max = sum;
+
+            }
+
+        }
+
+        return max;
+}
 }
